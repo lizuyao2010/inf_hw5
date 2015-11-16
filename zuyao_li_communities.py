d4239cf102317ef6eaafd8bcfc7c3c0905c8ee1c
lizuyao2010 <zuyaoli2010@gmail.com>
2015-11-15 18:22:39 -0800

    fix bugs but draw grey graph

--- zuyao_li_communities.py
+++ zuyao_li_communities.py
@@ -4,8 +4,9 @@ import sys
 import community
 import operator
 
-def set_partition(partition,n):
-    for node in n.neighbors():
+def set_partition(G,partition,n):
+    partition[n]=n
+    for node in G.neighbors(n):
         partition[node] = n
 
 def best_partition(G):
@@ -14,22 +15,20 @@ def best_partition(G):
     partition={}
     for node in G.nodes():
         partition[node]=1
-    n_edges=G.size # number of edges
+    n_edges=G.size() # number of edges
     best=-1
-    best_partition={}
-    for i in range(n_edges):
+    for i in xrange(n_edges-1):
         # edges_betweenness:
         #   Dictionary of edges with betweenness centrality as the value
         edges_betweenness=nx.edge_betweenness_centrality(G)
-        # for key in edges_betweenness:
         to_remove=max(edges_betweenness.iteritems(), key=operator.itemgetter(1))[0]
         G.remove_edge(to_remove[0],to_remove[1])
-        set_partition(partition,to_remove[0])
-        set_partition(partition,to_remove[1])
+        set_partition(G,partition,to_remove[0])
+        set_partition(G,partition,to_remove[1])
         current=community.modularity(partition, G)
         if current>best:
             best=current
-            best_partition=partition
+            best_partition=partition.copy()
     return best_partition
 
 def main():
@@ -48,11 +47,9 @@ def main():
     count = 0.
     for com in set(partition.values()) :
         count = count + 1.
-        list_nodes = [nodes for nodes in partition.keys()
-                                    if partition[nodes] == com]
+        list_nodes = [nodes for nodes in partition.keys() if partition[nodes] == com]
         print list_nodes
-        nx.draw_networkx_nodes(G, pos, list_nodes, node_size = 20,
-                                    node_color = str(count / size))
+        nx.draw_networkx_nodes(G, pos, list_nodes, node_size = 500, node_color = str(count / size))
     nx.draw_networkx_edges(G,pos, alpha=0.5)
     plt.show()
     plt.savefig(image_name)
